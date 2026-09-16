55e08aa742d7da33693ce52c1dac9a25b8fbb989
Ubuntu <ubuntu@ip-172-31-18-99.ap-northeast-1.compute.internal>
2026-09-16 08:40:41 +0000

    [finish]4-バリデーション

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,8 +6,11 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = Current.user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  validates :shop_name, presence: true
+  validates :caption, presence: true
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
